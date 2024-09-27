a96406a8aaae4923e7202fd78b702c95908ef7ad
Mahamud Osman <mahamud@Mahamuds-Laptop.local>
2024-09-27 19:19:55 -0400

    Question 2

---
+++ HW1/sumElements.py
@@ -0,0 +1,22 @@
+"""
+Sum of Elements in an Array
+Write a function that computes the sum of all elements in an array using array indexing.
+nums = [0,1,2,3,4]
+"""
+def sumElements(nums):
+    
+    if len(nums) == 0:
+        return -1
+    currSum = 0
+    for i in range(len(nums)):
+        currSum += nums[i]
+    print('Sum:', currSum)
+    
+    return currSum
+    
+    # Loop through all the elemets using index
+    # Add the element into the array
+    
+
+result = sumElements([10,20,30,40,50])
+assert result == 150, f'excpected 150 but got {result}'
