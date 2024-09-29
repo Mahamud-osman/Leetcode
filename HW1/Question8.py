c2195508728ba3c7c7f4c195285c0abfdc9669c9
Mahamud Osman <mahamud@Mahamuds-Laptop.local>
2024-09-28 21:21:59 -0400

    Question 9 skeleton

--- HW1/Question8.py
+++ HW1/Question8.py
@@ -3,7 +3,7 @@
 Write a function to rotate an array to the left by k positions. Use modular arithmetic to handle the array indexing.
 """
 def rotatebyK(nums, k): 
-    for i in range(len(nums)- 1, -1, -1):
+    for i in range(len(nums)- 1):
         while i < len(nums):        #Trying to handle out of bound error
             nums[i] = nums[i -k]
         
@@ -13,5 +13,5 @@ def rotatebyK(nums, k):
 
 
 #Manual testing:
-result == rotatebyK([1,3,5,7,11], 2)
+result = rotatebyK([1,3,5,7,11], 2)
 assert result == [1,3,5,7,11], f'Expected [1,3,5,7,11]. but got {result}'
--- HW1/Question9.py
+++ HW1/Question9.py
@@ -2,3 +2,13 @@
 9. Insert an Element at a Specific Index
 Write a function that inserts a new element at a specific index in the array, shifting the other elements to the right.
 """
+
+def specificIndex(nums, k, val): 
+    pass
+
+
+
+
+#manual Testing 
+result = specificIndex([1,3,5,7,11], 2, 7)
+assert result == [1,3,7,5,7,11], f'Expected [1,3,7,5,7,11], but got {result}'
